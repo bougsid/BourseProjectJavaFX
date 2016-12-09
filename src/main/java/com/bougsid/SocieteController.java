45b012c81d878a916174ce0df394795752bb6de3
Ayoub BOUGSID <ayoub.bougsid@gmail.com>
2016-12-09 14:45:05 +0000

    Add Pagination to societe and ordre

--- src/main/java/com/bougsid/SocieteController.java
+++ src/main/java/com/bougsid/SocieteController.java
@@ -9,10 +9,9 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import org.springframework.data.domain.Page;
 
 import java.net.MalformedURLException;
 import java.rmi.Naming;
@@ -22,11 +21,19 @@ import java.text.DecimalFormat;
 import java.util.Map;
 
 public class SocieteController {
+    private static final int MAX_SOCIETE_ITEMS = 4;
+    private static final int MAX_ORDRE_ITEMS = 10;
     @FXML
     private TextField searchTextField;
 
     @FXML
     private TableView<Societe> societeTable;
+
+    @FXML
+    private Pagination pagination;
+
+    @FXML
+    private Pagination ordrePagination;
 
     @FXML
     private TableColumn<Societe, String> codeColumn;
@@ -63,30 +70,60 @@ public class SocieteController {
 
     private ISocieteMetier societeMetier;
     private ObservableList<Societe> societes;
+    private Societe selectedSociete;
     private static final String RMI_SERVICE_URL = "rmi://localhost:1999/societeRmiService";
 
     @FXML
     private void initialize() throws RemoteException, NotBoundException, MalformedURLException {
+        this.societeMetier = (ISocieteMetier) Naming.lookup(RMI_SERVICE_URL);
+        this.configurePagination();
         //Set cell factory for SocieteTable
         this.setSocieteTableCellFactory();
         //Get Societe List
-        this.getSocieteList();
+        this.getSocieteList(0);
+        //Pagination factory
+        this.configurePaginationPageFactory();
         //Set Cell Factory fo OrdreTable
         this.setOrdreTableCellFactory();
         //Add listner to row clicked
         this.societeTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             try {
-                this.updateOrdresAndInfos(newValue);
+                this.selectedSociete = newValue;
+                this.updateOrdresAndInfos(0);
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
         });
     }
 
+    private void configurePaginationPageFactory(){
+        //Configure pagination
+        this.pagination.setPageFactory(param -> {
+            this.getSocieteList(param);
+            return new BorderPane(societeTable);
+        });
+        this.ordrePagination.setPageFactory(param -> {
+            this.updateOrdre(param);
+            return new BorderPane(ordreTable);
+        });
+    }
     private void setOrdreTableCellFactory() {
         this.actionsCountColumn.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getActionsCount()));
         this.actionPriceColumn.setCellValueFactory(cellData -> new SimpleDoubleProperty(cellData.getValue().getActionPrice()));
         this.dateColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDate()));
+        this.ordreTable.setRowFactory(tv -> new TableRow<Ordre>() {
+            @Override
+            public void updateItem(Ordre item, boolean empty) {
+                super.updateItem(item, empty);
+                if (item == null) {
+                    setStyle("");
+                } else if (item.getType().equals("Achat")) {
+                    setStyle("-fx-background-color: green;");
+                } else {
+                    setStyle("-fx-background-color: red;");
+                }
+            }
+        });
     }
 
     private void setSocieteTableCellFactory() {
@@ -94,16 +131,26 @@ public class SocieteController {
         this.nameColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getName()));
     }
 
-    private void getSocieteList() throws RemoteException, NotBoundException, MalformedURLException {
-        this.societeMetier = (ISocieteMetier) Naming.lookup(RMI_SERVICE_URL);
-        this.societes = FXCollections.observableArrayList(societeMetier.findAll(0, 20).getContent());
+    private void getSocieteList(int page) {
+        System.out.println("Page = " + page);
+        try {
+            this.societes = FXCollections.observableArrayList(this.societeMetier.findAll(page, MAX_SOCIETE_ITEMS).getContent());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
         this.societeTable.setItems(this.societes);
+        this.societeTable.getSelectionModel().select(0);
+        this.selectedSociete = this.societeTable.getSelectionModel().getSelectedItem();
     }
 
-    private void updateOrdresAndInfos(Societe societe) throws RemoteException {
-        if (societe == null) return;
-        this.ordreTable.setItems(FXCollections.observableList(this.societeMetier.getOrdresPage(societe.getCode(), 0, 5).getContent()));
-        Map<String, Double> infos = this.societeMetier.getAllInfos(societe.getCode());
+    private void configurePagination() throws RemoteException {
+        this.pagination.setPageCount(this.societeMetier.findAll(0, MAX_SOCIETE_ITEMS).getTotalPages());
+    }
+
+    private void updateOrdresAndInfos(int page) throws RemoteException {
+        if (selectedSociete == null) return;
+        this.updateOrdre(page);
+        Map<String, Double> infos = this.societeMetier.getAllInfos(selectedSociete.getCode());
         DecimalFormat df = new DecimalFormat("#.##");
         this.totalAchat.setText(String.valueOf(df.format(infos.get("totalAchat"))));
         this.totalVente.setText(String.valueOf(df.format(infos.get("totalVente"))));
@@ -112,10 +159,29 @@ public class SocieteController {
         this.estimation.setText(String.valueOf(df.format(infos.get("estimation"))));
     }
 
+    private void updateOrdre(int page) {
+        Page<Ordre> ordrePage = null;
+        try {
+            ordrePage = this.societeMetier.getOrdresPage(selectedSociete.getCode(), page, MAX_ORDRE_ITEMS);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        int totalPages = ordrePage.getTotalPages();
+        if (page == 0)
+            this.configureOrdrePagination(totalPages);
+        this.ordreTable.setItems(FXCollections.observableList(ordrePage.getContent()));
+    }
+
+    private void configureOrdrePagination(int totalPages) {
+        this.ordrePagination.setPageCount(totalPages);
+        this.ordrePagination.setCurrentPageIndex(0);
+    }
+
     @FXML
     void search() throws RemoteException {
         String code = this.searchTextField.getText();
         this.societeTable.getSelectionModel().clearSelection();
-        this.updateOrdresAndInfos(this.societeMetier.findSocieteByCode(code));
+        this.selectedSociete = this.societeMetier.findSocieteByCode(code);
+        this.updateOrdresAndInfos(0);
     }
 }
